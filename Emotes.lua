913975042fb795295d40374d55c50b9703f2ae14
Fredles <fboisvert@outlook.com>
2022-12-09 12:38:45 -0500

    Initial Commit + passuperXD

---
+++ Emotes.lua
@@ -0,0 +1,7 @@
+TwitchEmotes_Blasphemy_Emoticons = {
+    ["passuperXD"] = "passuperXD",
+}
+
+TwitchEmotes_Blasphemy_Emoticons_Pack = {
+    ["passuperXD"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\passuperXD.tga:28:28"
+}
---
+++ Init_Emotes.lua
@@ -0,0 +1,5 @@
+local function addEmotePacks()
+    Emoticons_RegisterPack("TwitchEmotes_Blasphemy", TwitchEmotes_Blasphemy_Emoticons, TwitchEmotes_Blasphemy_Emoticons_Pack)
+end
+
+addEmotePacks()
