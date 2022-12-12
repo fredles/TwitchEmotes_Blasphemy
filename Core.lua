405a648c14f18692579703cd4ad076ef95d3898d
Fredles <fboisvert@outlook.com>
2022-12-11 23:05:45 -0500

    add dependency def

--- Core.lua
+++ Core.lua
@@ -1,6 +1,5 @@
 ---@diagnostic disable: deprecated
 TwitchEmotes_Blasphemy = LibStub("AceAddon-3.0"):NewAddon("TwitchEmotes_Blasphemy", "AceConsole-3.0", "AceEvent-3.0")
-local AddonName = ...
 
 --Init
 function TwitchEmotes_Blasphemy:OnInitialize()
