c79ad879f5f6c53c77ef17e0b7df3a5aa52fab1c
VMaltais <36899853+VMaltais@users.noreply.github.com>
2022-12-12 16:22:45 -0500

    Added emotes definitions

--- Emotes.lua
+++ Emotes.lua
@@ -1,11 +1,45 @@
 TwitchEmotes_Blasphemy_Emoticons = {
     ["pasSuperXD"] = "pasSuperXD",
     ["notBadCat"] = "notBadCat",
-    ["PasSuperLMAO"] = "PasSuperLMAO"
+    ["PasSuperLMAO"] = "PasSuperLMAO",
+    ["%"] = "%",
+    ["10"] = "10",
+    ["Blaze"] = "Blaze",
+    ["BlokChamp"] = "BlokChamp",
+    ["BlokSleeper"] = "BlokSleeper",
+    ["cuteAF"] = "cuteAF",
+    ["falcoBathroom"] = "falcoBathroom",
+    ["FeelsAzsharaMan"] = "FeelsAzsharaMan",
+    ["Fredge"] = "Fredge",
+    ["gino"] = "gino",
+    ["kmantaloup"] = "kmantaloup",
+    ["Leguman"] = "Leguman",
+    ["lePepe"] = "lePepe",
+    ["quilleur"] = "quilleur",
+    ["quilleW"] = "quilleW",
+    ["saddBepis"] = "saddBepis",
+    ["Ultramar"] = "Ultramar"
 }
 
 TwitchEmotes_Blasphemy_Emoticons_Pack = {
     ["pasSuperXD"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\pasSuperXD.tga:28:28",
     ["notBadCat"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\notBadCat.tga:28:28",
-    ["PasSuperLMAO"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\PasSuperLMAO.tga:28:28"
+    ["PasSuperLMAO"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\PasSuperLMAO.tga:28:28",
+    ["%"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\%.tga:28:28",
+    ["10"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\10.tga:28:28",
+    ["Blaze"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\Blaze.tga:28:28",
+    ["BlokChamp"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\BlokChamp.tga:28:28",
+    ["BlokSleeper"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\BlokSleeper.tga:28:28",
+    ["cuteAF"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\cuteAF.tga:28:28",
+    ["falcoBathroom"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\falcoBathroom.tga:28:28",
+    ["FeelsAzsharaMan"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\FeelsAzsharaMan.tga:28:28",
+    ["Fredge"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\Fredge.tga:28:28",
+    ["gino"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\gino.tga:28:28",
+    ["kmantaloup"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\kmantaloup.tga:28:28",
+    ["Leguman"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\Leguman.tga:28:28",
+    ["lePepe"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\lePepe.tga:28:28",
+    ["quilleur"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\quilleur.tga:28:28",
+    ["quilleW"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\quilleW.tga:28:28",
+    ["saddBepis"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\saddBepis.tga:28:28",
+    ["Ultramar"] = "Interface\\AddOns\\TwitchEmotes_Blasphemy\\emotes\\Ultramar.tga:28:28"
 }
